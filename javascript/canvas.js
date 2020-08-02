a39d802331e11f7ceec435d595a52e195227c1e9
Blakemcw <blakecmcwilliam@gmail.com>
2020-08-01 19:24:34 -0700

    Add code to render a blank cavas with p5.js

---
+++ javascript/canvas.js
@@ -0,0 +1,18 @@
+var w = window.innerWidth
+     || document.documentElement.clientWidth
+     || document.body.clientWidth;
+
+var h = window.innerHeight
+     || document.documentElement.clientHeight
+     || document.body.clientHeight; 
+
+
+function setup () {
+    createCanvas(w, h);
+    background(0);
+    fullscreen();
+}
+
+function draw () {
+    noStroke();
+}
